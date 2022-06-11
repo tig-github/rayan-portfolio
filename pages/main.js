a8c9c11dee6d770df033079f53634bab51638009
Rayan Tighiouart <t.alex.2002@gmail.com>
2022-06-10 20:26:35 -0700

    darkmode now stays between pages using localstorage

--- pages/main.js
+++ pages/main.js
@@ -1,18 +1,20 @@
-//cookies are not supported with github pages, so for now alternatives needed
-let darkmode = false;
+function initialize(){
+    let darkmode = true;
+    localStorage.setItem('bool', true);
+}
 
+function startUp() {
+    //on startup of each page to maintain consistency
+    var darkbutton = document.getElementById("darkbutton");
+    var all = document.getElementsByTagName("*");
 
-function DarkMode() {
-    let darkbutton = document.getElementById("darkbutton");
-    let all = document.getElementsByTagName("*");
-
-
-    if (darkmode){
+    darkmode = localStorage.getItem('bool');
+    if (darkmode === 'true'){
         //set it to light
-        document.documentElement.style.setProperty("--background", "url(../images/day.jpg)");
         document.documentElement.style.setProperty("--text-color", "black");
         document.documentElement.style.setProperty("--section-color", "rgb(173, 255, 201)");
         document.documentElement.style.setProperty("--loading-color", "navy");
+        document.documentElement.style.setProperty("--background", "url(../images/day.jpg)");
 
 
         document.documentElement.style.setProperty("--table-side-color", "rgb(173, 255, 201)");
@@ -27,10 +29,10 @@ function DarkMode() {
     }
     else {
         //set it to dark
-        document.documentElement.style.setProperty("--background", "url(../images/night.jpg)");
         document.documentElement.style.setProperty("--text-color", "white");
         document.documentElement.style.setProperty("--section-color", "rgb(81, 13, 116)");
         document.documentElement.style.setProperty("--loading-color", "rgb(30, 0, 85)");
+        document.documentElement.style.setProperty("--background", "url(../images/night.jpg)");
 
         document.documentElement.style.setProperty("--table-side-color", "rgb(81, 13, 116)");
         document.documentElement.style.setProperty("--table-row-light", "rgb(22, 22, 94)");
@@ -42,7 +44,57 @@ function DarkMode() {
         darkbutton.textContent = "☀️";
         //document.cookie = "darkmode=true"; add cookies once hosted
     }
+}
 
-    darkmode = !darkmode;
+
+function DarkMode() {
+    var darkbutton = document.getElementById("darkbutton");
+    var all = document.getElementsByTagName("*");
+
+    //let key = Object.keys(localStorage)
+    darkmode = localStorage.getItem('bool');
+    if (darkmode === 'false'){
+        //set it to light
+        document.documentElement.style.setProperty("--text-color", "black");
+        document.documentElement.style.setProperty("--section-color", "rgb(173, 255, 201)");
+        document.documentElement.style.setProperty("--loading-color", "navy");
+        document.documentElement.style.setProperty("--background", "url(../images/day.jpg)");
+
+
+        document.documentElement.style.setProperty("--table-side-color", "rgb(173, 255, 201)");
+        document.documentElement.style.setProperty("--table-row-light", "rgb(191, 234, 255)");
+        document.documentElement.style.setProperty("--table-row-dark", "rgb(190, 222, 255)");
+
+        document.documentElement.style.setProperty("--link-color", "blue");
+        document.documentElement.style.setProperty("--link-visited", "purple");
+
+        darkbutton.textContent = "🌑";
+
+        localStorage.clear();
+        localStorage.setItem('bool', true);
+        //document.cookie = "darkmode=false"; add cookies once hosted
+    }
+    else {
+        //set it to dark
+        document.documentElement.style.setProperty("--text-color", "white");
+        document.documentElement.style.setProperty("--section-color", "rgb(81, 13, 116)");
+        document.documentElement.style.setProperty("--loading-color", "rgb(30, 0, 85)");
+        document.documentElement.style.setProperty("--background", "url(../images/night.jpg)");
+
+
+        document.documentElement.style.setProperty("--table-side-color", "rgb(81, 13, 116)");
+        document.documentElement.style.setProperty("--table-row-light", "rgb(22, 22, 94)");
+        document.documentElement.style.setProperty("--table-row-dark", "rgb(6, 6, 71)");
+
+        document.documentElement.style.setProperty("--link-color", "rgb(247, 100, 132)");
+        document.documentElement.style.setProperty("--link-visited", "rgb(155, 155, 70)");
+
+        darkbutton.textContent = "☀️";
+
+        localStorage.clear();
+        localStorage.setItem('bool', false);
+        //document.cookie = "darkmode=true"; add cookies once hosted
+    }
+    //darkmode = !darkmode;
 }
 
