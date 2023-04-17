ec3981b984718b0ed76ccfaa5698ea791f50a8a3
Rayan Tighiouart <t.alex.2002@gmail.com>
2023-04-16 19:57:41 -0700

    information on projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -10,14 +10,38 @@ const testText =
 const personalText =
   "These are personal projects I have created on my own time, generally to get a greater understanding of an area of Computer Science or build something interesting. Examples include this website, the data structures mini projects, and more.";
 const courseText =
-  "These are the various projects I have built during my time at UCI, many of which in dedicated project courses on specialized topics.";
+  "These are the various projects I have built during my time at UCI, many of which in dedicated project courses on specialized topics. Unfortunately most of the repositories must be kept private so the code itself does not spread, but the following are projects I have worked on.";
 const volunteerText =
   "These are projects I have contributed to as part of the organization Commit the Change for non profit organizations.";
 
+const personalItems = {
+  Yazify:
+    "This is a web app that allows users to input a spotify song, and get 5 recommendations based on a large index. Currently I am planning to tune the recommendation algorithm and introduce a significantly larger index, and then plan to add more features such as recommendations based on playlists/user profiles, saving recommendations, etc. The algorithm currently is cosine similarity, I plan to introduce more sophisticated algorithms once the index size grows. ",
+  "DSA Mini Projects":
+    "This is a series of small projects that I work on whenever I want to do something interesting with a particular data structure or algorithm. Currently I implemented a Turing Machine in Python with a self implemented Linked List, and am working on a Persistent RedBlack Tree in C++ for point location, along with a small Computational Geometry library.",
+  GreedyAnts:
+    "For HackUCI 2022, built when my team and I had no web development experience.",
+};
+const personalLinks = {
+  Yazify: "https://github.com/tig-github/yazify",
+  "DSA Mini Projects": "https://github.com/tig-github/DSA_miniprojects",
+  GreedyAnts: "https://github.com/tig-github/GreedyAnts",
+};
+
+const courseItems = {
+  "CS 121 Search Engine":
+    "This was a fully functioning search engine for CS 121 - Information Retrieval",
+  "ICS 51 Wordle in Assembly":
+    "A fully functioning game of Wordle built entirely in MIPS assembly",
+};
+
 const socialItems = {
-  "Patriots and Paws": "This is for patriots and paws",
-  "Abound Food Care": "This is for abound food care",
-  "Women's Safety Website": "This is for women's safety website",
+  "Patriots and Paws":
+    "This is a website I am currently working on as part of the club CTC for the PNP nonprofit in Orange County. I have helped implement several frontend and backend features such as the navbar, SQL DDLs and routes, and pagination. Currently the project is in end stages.",
+  "Abound Food Care":
+    "I worked on this website as part of a small team during the summer, and helped implement pagination and several frontend features.",
+  "Women's Safety Website":
+    "This was the first website I worked on as part of a small team of five to help provide resources for women on campus. Ultimately the team dissolved before the project could be finished, but I gained valuable experience.",
 };
 const socialLinks = {
   "Patriots and Paws": "https://github.com/ctc-uci/patriots-and-paws-frontend",
@@ -36,11 +60,14 @@ const Projects = () => {
           text={personalText}
           header={"Personal"}
           image={personalImage}
+          listItems={personalItems}
+          listLinks={personalLinks}
         ></BoxField>
         <BoxField
           text={courseText}
           header={"Coursework"}
           image={courseImage}
+          listItems={courseItems}
         ></BoxField>
         <BoxField
           text={volunteerText}
