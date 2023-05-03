34d1f736d65c8ce11a5f8baa37cc30e56ef53aa1
Rayan Tighiouart <t.alex.2002@gmail.com>
2023-05-02 19:39:15 -0700

    changed homepage homefield

--- src/components/HomeField.jsx
+++ src/components/HomeField.jsx
@@ -17,26 +17,23 @@ const HomeField = () => {
   return (
     <>
       <Box bg="#DCCAE9" ml=".5rem" mb="2rem" pb="2rem" mt="2rem" w="100%">
-        <Flex>
-          <Stack mt="1rem" ml="5.5rem" spacing={5}>
-            <Heading>Hello! My name is Rayan</Heading>
-            <Text>
-              I am currently a Computer Science student at the University of
-              California, Irvine
-            </Text>
-            <Text>I am passionate about Music, History, and Software.</Text>
-            <Text>
-              I like learning about Algorithms, Programming Languages, and more.
-            </Text>
-            <Text>
-              Portfolio Todo: 3d Yaz with Threejs, display github commit history
-              right here.
-            </Text>
-            <Text>Right now I'll cheat and use a screenshot ;)</Text>
-          </Stack>
-          <Spacer />
-          <Image src={commit} w="45rem" h="9rem" mt="3rem" mr="3rem" />
-        </Flex>
+        <Stack align="center" mt="1rem" ml="5.5rem" spacing={5}>
+          <Heading mb="2rem">
+            <Text>Hello! My name is Rayan</Text>
+          </Heading>
+          <Text>
+            I am currently a Computer Science student at the University of
+            California, Irvine
+          </Text>
+          <Text>I am passionate about Music, History, and Software.</Text>
+          <Text>
+            I like learning about Algorithms, Programming Languages, and more.
+          </Text>
+          <Text>
+            Portfolio Todo: 3d Yaz with Threejs, display github commit history
+            right here.
+          </Text>
+        </Stack>
       </Box>
     </>
   );
