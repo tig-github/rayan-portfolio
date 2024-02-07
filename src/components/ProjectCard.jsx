871002bfd086e64dd83531caf16dffaa112758b2
Rayan Tighiouart <t.alex.2002@gmail.com>
2024-02-07 01:21:30 -0800

    ProjectCard component to display projects nicer

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,35 @@
+/*
+    Card that gives information on a given project
+*/
+import React from "react";
+import {
+  Box,
+  Text,
+  Stack,
+  Flex,
+  Spacer,
+  Heading,
+  Image,
+  Card,
+  CardHeader, 
+  CardBody, 
+  CardFooter
+  
+} from "@chakra-ui/react";
+
+const ProjectCard = () => {
+    return (
+    <>
+        <Card>
+            <CardHeader>
+            </CardHeader>
+            <CardBody>
+            </CardBody>
+            <CardFooter>
+            </CardFooter>
+        </Card>
+    </>
+    )
+}
+
+export default ProjectCard;
