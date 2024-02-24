0d2377f01f5b37b2d3090759411080db0c4ffd6f
Rayan Tighiouart <t.alex.2002@gmail.com>
2024-02-23 23:19:56 -0800

    figured out color changes for heatmap

--- src/components/Commits/Commits.jsx
+++ src/components/Commits/Commits.jsx
@@ -2,33 +2,45 @@
 import React from 'react';
 import { 
     Box,
-    useBreakpointValue,
   } from "@chakra-ui/react";
 import CalendarHeatmap from 'react-calendar-heatmap';
 import 'react-calendar-heatmap/dist/styles.css';
 import './Commits.css';
 
+// simple function returning color values
+const colorValue = (value) => {
+    if (!value) {
+        return 'color-empty';
+    }
+    if (value.count < 3) {
+        return 'lowcommit';
+    }
+    if (value.count < 8) {
+        return 'mediumcommit';
+    }
+    if (value.count < 15) {
+        return 'highcommit';
+    }
+    return 'hugecommit';
+}
+
+
 const Commits = () => {
     return (
         <>
             <Box w="50%">
-            <CalendarHeatmap
-                startDate={new Date('2016-01-01')}
-                endDate={new Date('2016-04-01')}
-                values={[
-                    { date: '2016-01-02', count: 12 },
-                    { date: '2016-01-22', count: 122 },
-                    { date: '2016-01-30', count: 38 },
-                    { date: '2016-01-31', count: 1 },
-                    // ...and so on
-                ]}
-                classForValue={(value) => {
-                    if (!value) {
-                      return 'color-empty';
-                    }
-                    return `color-scale-${value.count}`;
-                  }}
-            />
+                <CalendarHeatmap
+                    startDate={new Date('2016-01-01')}
+                    endDate={new Date('2016-04-01')}
+                    values={[
+                        { date: '2016-01-02', count: 12 },
+                        { date: '2016-01-22', count: 122 },
+                        { date: '2016-01-30', count: 38 },
+                        { date: '2016-01-31', count: 1 },
+                        // ...and so on
+                    ]}
+                    classForValue={(value) => colorValue(value)}
+                />
             </Box>
 
         </>
