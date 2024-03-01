78b79bfbfa49dc62678dcbcd3f2c200163036fe4
Rayan Tighiouart <t.alex.2002@gmail.com>
2024-03-01 12:51:58 -0800

    commit heatmap cached

--- src/utils/github.js
+++ src/utils/github.js
@@ -4,7 +4,38 @@
 import { Octokit } from "octokit";
 
 
-// test function that gets issues from a repository
+
+/* 
+gets commits from github user tig-github from 2024
+sends an unauthenticated request meant to run on deployment
+*/
+export const getCommits = async () => {
+    const octokit = new Octokit();
+    const query_string = 'q=' + encodeURIComponent(`author:tig-github author-date:2024-01-01..2024-12-31`);
+    const commitData = await octokit.paginate(`GET /search/commits?${query_string}`, {
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28'        
+        },
+    });
+    return commitData;
+}
+
+/* 
+gets commits from 2021-2023 and saves them to JSON
+sends an authenticated request meant only to run for processing prior to deployment
+*/
+export const getPriorCommits = async () => {
+    const octokit = new Octokit({ auth: process.env.REACT_APP_GITHUB_TOKEN });
+    const query_string = 'q=' + encodeURIComponent(`author:tig-github author-date:2021-01-01..2024-01-01`);
+    const commitData = await octokit.paginate(`GET /search/commits?${query_string}`, {
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28'        
+        },
+    });
+    return commitData;
+}
+
+// test function that gets issues from a repository, used for testing the github API
 export const getIssues = async () => {
     const octokit = new Octokit({ auth: process.env.REACT_APP_GITHUB_TOKEN });
     const issueData = await octokit.paginate("GET /repos/{owner}/{repo}/issues", {
@@ -19,22 +50,9 @@ export const getIssues = async () => {
     return issueData;
 }
 
-// gets commits from github user tig-github
-export const getCommits = async (year) => {
-    const octokit = new Octokit();
-    const query_string = 'q=' + encodeURIComponent(`author:tig-github author-date:${year}-01-01..${year}-12-31`);
-    const commitData = await octokit.paginate(`GET /search/commits?${query_string}`, {
-        headers: {
-          'X-GitHub-Api-Version': '2022-11-28'        
-        },
-    });
-    return commitData;
-}
-
 // creates dict of form {date: count} then reprocesses into format of [...{date: date, count: count}]
 export const processCommitCounts = (commits) => {
   let counts = {};
-  console.log(commits)
   commits.forEach(
       (commit) => {
           let commitDate = (commit['commit']['author']['date']).slice(0,10);
@@ -46,12 +64,11 @@ export const processCommitCounts = (commits) => {
           }
       }
   )
-  console.log(counts);
   return counts;
 }
 
 // turns the count dict into an array of individual values
-export const processCommitValues = (counts) => {
+export const processCommitValues = counts => {
     let vals = [];
     Object.keys(counts).map((commitDate) => {
         vals.push({date: commitDate, count: counts[commitDate]});
