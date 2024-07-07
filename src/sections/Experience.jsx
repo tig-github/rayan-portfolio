fc887cb12d1b7774fa1ab2adeafbb11768b349fd
Rayan Tighiouart <t.alex.2002@gmail.com>
2024-07-07 15:04:58 -0700

    basic timeline component

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,9 +1,14 @@
 import React from "react";
-import { Box, Text, Heading, Stack } from "@chakra-ui/react";
+import { Box, Text, Heading, Stack, Image } from "@chakra-ui/react";
 import {
   VerticalTimeline,
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { FaReact } from "react-icons/fa";
+import ctcIcon from "../images/CTC.svg";
+//import CtcIcon from "../components/Icons/ctcIcon";
+
 const Experience = () => {
   return (
     <>
@@ -17,6 +22,29 @@ const Experience = () => {
           <Heading as="h1" mb="4rem">
             <Text color="white">Experience</Text>
           </Heading>
+          <VerticalTimeline>
+            <VerticalTimelineElement
+              className="vertical-timeline-element--work"
+              contentStyle={{ background: "white", color: "#780be6" }}
+              contentArrowStyle={{
+                borderRight: "7px solid white",
+              }}
+              date="Oct 2021 - June 2024"
+              icon={<Image src={ctcIcon} borderRadius="full" />}
+            >
+              <h3 className="vertical-timeline-element-title">
+                Student Volunteer Software Developer
+              </h3>
+              <h4 className="vertical-timeline-element-subtitle">
+                Commit the Change
+              </h4>
+              <Text>
+                Developer in student organization creating open source software
+                for local nonprofits utilizing common industry standards, with
+                contributions to several projects.
+              </Text>
+            </VerticalTimelineElement>
+          </VerticalTimeline>
         </Stack>
       </Box>
     </>
